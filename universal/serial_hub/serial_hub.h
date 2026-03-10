47e5afb3e78f6b63d9a03922559c83b12b23a313
Febri Bayu Nurcahyo <febribayun@gmail.com>
2026-03-11 00:16:02 +0700

    fix: i get wiser.

--- universal/serial_hub/serial_hub.h
+++ universal/serial_hub/serial_hub.h
@@ -1,6 +1,7 @@
 #ifndef SERIAL_HUB_H
 #define SERIAL_HUB_H
 
+#include <stdio.h>
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -12,7 +13,7 @@ typedef uint16_t fsize_t;
 
 typedef uint16_t fssize_t;
 
-typedef void (*on_receive_cb_t)(uint8_t *data, fsize_t size);
+typedef void (*on_receive_cb_t)(void *ctx, uint8_t *data, fsize_t size);
 
 typedef void (*write_cb_t)(void *ctx, uint8_t *data, fsize_t size);
 
@@ -23,30 +24,31 @@ typedef struct {
 } __attribute__((packed)) serial_hub_raw_packet;
 
 typedef struct {
-  fsize_t max_length;
   fsize_t expected_length;
   int8_t state;
   on_receive_cb_t callback;
+  void *ctx;
 } serial_hub_topic_t;
 
 typedef struct {
   int8_t state;
-  uint8_t current_topic;
 
   uint8_t *__read_buf;
   uint8_t *__write_buf;
-  uint8_t *__cobsr_tmp_buf;
-  fsize_t __read_buf_size;
-  fsize_t __write_buf_size;
-  fsize_t __cobsr_tmp_buf_size;
 
-  fsize_t __expected_length;
-  fsize_t __real_max_length;
   void *__write_cb_context;
 
-  uint16_t __count;
+  fsize_t __read_buf_size;
+  fsize_t __write_buf_size;
+
+  fsize_t __count;
+  fsize_t __next_zero;
+
   write_cb_t __write_cb;
+
+  serial_hub_topic_t *__current_topic;
   serial_hub_topic_t topics[SERIAL_HUB_TOPIC_MAX];
+
 } serial_hub_handle_t;
 
 #define SERIAL_HUB_ERR_INVALID_SIZE -1
@@ -60,11 +62,9 @@ typedef struct {
 #define SERIAL_HUB_STATE_UNITIALIZED 2
 #define SERIAL_HUB_STATE_DEINITIALIZED -1
 
-#define SERIAL_HUB_READ_STATE_HIT_FIRST_ZERO 3
-#define SERIAL_HUB_READ_STATE_WAITING_FOR_LENGTH 4
-#define SERIAL_HUB_READ_STATE_WAITING_FOR_LENGTH_MSB 5
-#define SERIAL_HUB_READ_STATE_READING 6
-#define SERIAL_HUB_READ_STATE_WAITING_FOR_ID 8
+#define SERIAL_HUB_READ_STATE_HIT_READING 6
+#define SERIAL_HUB_READ_STATE_HIT_ZERO 8
+#define SERIAL_HUB_READ_STATE_HIT_ID 9
 #define SERIAL_HUB_READ_STATE_EMPTY 7
 
 #define SERIAL_HUB_OK 0
@@ -73,7 +73,8 @@ void serial_hub_on_read(serial_hub_handle_t *handle, uint8_t *data,
                         fsize_t size);
 
 int8_t serial_hub_attach_topic(serial_hub_handle_t *handle, uint8_t id,
-                               fsize_t size, on_receive_cb_t callback);
+                               fsize_t size, void *ctx,
+                               on_receive_cb_t callback);
 
 int8_t serial_hub_dettach_topic(serial_hub_handle_t *handle, uint8_t id,
                                 on_receive_cb_t callback);
@@ -88,94 +89,6 @@ int8_t serial_hub_initialize(serial_hub_handle_t *handle, write_cb_t write_cb,
 
 void serial_hub_reserve_memory(serial_hub_handle_t *handle,
                                fsize_t max_message_length);
-
-/*****************************************************************************
- *
- * cobsr.h
- *
- * Consistent Overhead Byte Stuffing--Reduced (COBS/R)
- *
- ****************************************************************************/
-
-/*****************************************************************************
- * Includes
- ****************************************************************************/
-
-#include <stdint.h>
-#include <stdlib.h>
-
-/*****************************************************************************
- * Defines
- ****************************************************************************/
-
-#define COBSR_ENCODE_DST_BUF_LEN_MAX(SRC_LEN)                                  \
-  (((SRC_LEN) == 0u) ? 1u : ((SRC_LEN) + (((SRC_LEN) + 253u) / 254u)))
-#define COBSR_DECODE_DST_BUF_LEN_MAX(SRC_LEN) (SRC_LEN)
-
-/*
- * For in-place encoding, the source data must be offset in the buffer by
- * the following amount (or more).
- */
-#define COBSR_ENCODE_SRC_OFFSET(SRC_LEN) (((SRC_LEN) + 253u) / 254u)
-
-/*****************************************************************************
- * Typedefs
- ****************************************************************************/
-
-typedef enum {
-  COBSR_ENCODE_OK = 0x00,
-  COBSR_ENCODE_NULL_POINTER = 0x01,
-  COBSR_ENCODE_OUT_BUFFER_OVERFLOW = 0x02
-} cobsr_encode_status;
-
-typedef struct {
-  fsize_t out_len;
-  cobsr_encode_status status;
-} cobsr_encode_result;
-
-typedef enum {
-  COBSR_DECODE_OK = 0x00,
-  COBSR_DECODE_NULL_POINTER = 0x01,
-  COBSR_DECODE_OUT_BUFFER_OVERFLOW = 0x02,
-  COBSR_DECODE_ZERO_BYTE_IN_INPUT = 0x04,
-} cobsr_decode_status;
-
-typedef struct {
-  fsize_t out_len;
-  cobsr_decode_status status;
-} cobsr_decode_result;
-
-/*****************************************************************************
- * Function prototypes
- ****************************************************************************/
-
-/* COBS/R-encode a string of input bytes, which may save one byte of output.
- *
- * dst_buf_ptr:    The buffer into which the result will be written
- * dst_buf_len:    Length of the buffer into which the result will be written
- * src_ptr:        The byte string to be encoded
- * src_len         Length of the byte string to be encoded
- *
- * returns:        A struct containing the success status of the encoding
- *                 operation and the length of the result (that was written to
- *                 dst_buf_ptr)
- */
-cobsr_encode_result cobsr_encode(void *dst_buf_ptr, fsize_t dst_buf_len,
-                                 const void *src_ptr, fsize_t src_len);
-
-/* Decode a COBS/R byte string.
- *
- * dst_buf_ptr:    The buffer into which the result will be written
- * dst_buf_len:    Length of the buffer into which the result will be written
- * src_ptr:        The byte string to be decoded
- * src_len         Length of the byte string to be decoded
- *
- * returns:        A struct containing the success status of the decoding
- *                 operation and the length of the result (that was written to
- *                 dst_buf_ptr)
- */
-cobsr_decode_result cobsr_decode(void *dst_buf_ptr, fsize_t dst_buf_len,
-                                 const void *src_ptr, fsize_t src_len);
 
 #ifdef __cplusplus
 } /* extern "C" */
--- universal/serial_hub/test.cpp
+++ universal/serial_hub/test.cpp
@@ -3,22 +3,46 @@
 #include <algorithm>
 #include <cstdint>
 #include <cstdio>
+#include <cstring>
 #include <random>
+#include <string.h>
 #include <vector>
 
-#define TEST_SIZE 30
+#define TEST_SIZE 24
+#include <stdint.h>
+#include <stdio.h>
+
+inline void print_hex_dump(const uint8_t *data, size_t size) {
+  for (fsize_t i = 0; i < size; i++) {
+    printf("%02X ", data[i]);
+
+    // Print a newline every 16 bytes for readability
+    if ((i + 1) % 16 == 0) {
+      printf("\n");
+    }
+  }
+  // Add a final newline if the last line didn't end perfectly on a 16-byte
+  // boundary
+  if (size % 16 != 0) {
+    printf("\n");
+  }
+}
 
 std::vector<uint8_t> write_buf;
 
 void local_write(void *ctx, uint8_t *data, fsize_t size) {
   serial_hub_handle_t *dst = (serial_hub_handle_t *)ctx;
-  write_buf.insert(write_buf.end(), data, data + size);
+  print_hex_dump(data, size);
+  fsize_t first = size / 4;
+  fsize_t middle = first * 3;
+  serial_hub_on_read(dst, data, size);
 };
 
 fsize_t well = 0;
-void local_on_read(uint8_t *data, fsize_t size) {
+void local_on_read(void *ctx, uint8_t *data, fsize_t size) {
   well++;
-  printf("%d reading data %d\n", well, size);
+  printf("\n%d reading data %d\n", well, size);
+  print_hex_dump(data, size);
 };
 
 int main(int argc, char *argv[]) {
@@ -27,37 +51,29 @@ int main(int argc, char *argv[]) {
 
   serial_hub_initialize(&handle1, local_write, &handle2);
   serial_hub_initialize(&handle2, local_write, &handle1);
+  serial_hub_reserve_memory(&handle1, TEST_SIZE);
+  serial_hub_reserve_memory(&handle2, TEST_SIZE);
 
-  uint8_t dat[TEST_SIZE] = {};
+  uint8_t dat[TEST_SIZE + 3] = {};
 
-  for (fsize_t t = 1; t < TEST_SIZE; t++) {
-    dat[t] = t % 254;
+  for (fsize_t t = 0; t < TEST_SIZE + 3; t++) {
+    dat[t] = (t % 254) + 1;
+    if (dat[t] % 5 == 0) {
+      dat[t] = 0x0;
+    }
   }
+
+  printf("\n");
+  printf("original:\n");
+  print_hex_dump(dat, TEST_SIZE);
+  printf("\n");
 
   // serial_hub_reserve_memory(&handle1, TEST_SIZE);
   // serial_hub_reserve_memory(&handle2, TEST_SIZE);
-  serial_hub_attach_topic(&handle2, 1, sizeof(dat), local_on_read);
 
-  for (fsize_t i = 0; i < 100; i++) {
-    serial_hub_write_topic(&handle1, 1, (uint8_t *)&dat, TEST_SIZE);
-  }
+  serial_hub_attach_topic(&handle2, 1, sizeof(dat), NULL, local_on_read);
 
-  std::random_device rd;
-
-  std::mt19937 gen(rd());
-
-  std::uniform_int_distribution<> distrib(25, 50);
-
-  fsize_t fuck = 0;
-  while (fuck < write_buf.size()) {
-
-    fsize_t advance = fuck + distrib(gen);
-    if (advance > write_buf.size()) {
-      advance = write_buf.size();
-    }
-    serial_hub_on_read(&handle2, &write_buf[fuck], advance - fuck);
-    fuck = advance;
-  }
+  serial_hub_write_topic(&handle1, 1, (uint8_t *)&dat, TEST_SIZE + 3);
 
   return 0;
 }
